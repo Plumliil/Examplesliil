d577a6e6dee6aa0d7144a9601c53b9f2d6872743
plumliil <476453574@qq.com>
2023-01-13 23:52:58 +0800

    feat: tools

---
+++ src/views/Tools.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="toolComponents[curComponent]"></component>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tools",
+};
+</script>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useRoute } from "vue-router";
+import { toolComponents } from "../route/examples";
+
+const route = useRoute();
+console.log(route.params.name);
+
+let curComponent = ref(route.params.name + "");
+console.log(curComponent);
+console.log(toolComponents);
+</script>
+
+<style scoped>
+body {
+  background-color: pink;
+}
+</style>
